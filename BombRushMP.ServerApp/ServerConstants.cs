e82bfaacb2bd7a1fd4bfdac3b2c6943c03372457
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-08-09 12:28:56 -0300

    add constant for player count tickrate

---
+++ BombRushMP.ServerApp/ServerConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombRushMP.ServerApp
+{
+    public static class ServerConstants
+    {
+        public const float PlayerCountTickRate = 1f;
+    }
+}
