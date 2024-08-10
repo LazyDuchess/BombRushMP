c2b85760a4c039286de7f373d3c085a30abd9e22
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-08-09 21:03:37 -0300

    Only let 1 button open the phone

--- BombRushMP.Plugin/Patches/PhonePatch.cs
+++ BombRushMP.Plugin/Patches/PhonePatch.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
 using Reptile.Phone;
+using System.Reflection.Emit;
 
 namespace BombRushMP.Plugin.Patches
 {
@@ -18,6 +19,28 @@ namespace BombRushMP.Plugin.Patches
             if (SpectatorController.Instance != null)
                 return false;
             return true;
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(Reptile.Phone.Phone.OpenCloseHandeling))]
+        private static IEnumerable<CodeInstruction> OpenCloseHandeling_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var pastCloseHandling = false;
+            foreach (var instruction in instructions)
+            {
+                if (instruction.LoadsConstant(57) && !pastCloseHandling)
+                {
+                    pastCloseHandling = true;
+                    yield return instruction;
+                    continue;
+                }
+                if (instruction.LoadsConstant(21) || instruction.LoadsConstant(29) || instruction.LoadsConstant(57))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_S, 21);
+                }
+                else
+                    yield return instruction;
+            }
         }
     }
 }
