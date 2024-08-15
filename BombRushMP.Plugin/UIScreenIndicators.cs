b2cd6cd5dd57380dac002df794cdec38ffcec1d3
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-08-15 17:30:59 -0300

    UI Screen Indicators: Display below everything else

--- BombRushMP.Plugin/UIScreenIndicators.cs
+++ BombRushMP.Plugin/UIScreenIndicators.cs
@@ -88,6 +88,7 @@ namespace BombRushMP.Plugin
             var prefab = mpAssets.Bundle.LoadAsset<GameObject>("Screen Indicators UI");
             var indicatorUI = Instantiate(prefab);
             indicatorUI.transform.SetParent(Core.Instance.UIManager.transform, false);
+            indicatorUI.transform.SetAsFirstSibling();
             return indicatorUI.AddComponent<UIScreenIndicators>();
         }
     }
