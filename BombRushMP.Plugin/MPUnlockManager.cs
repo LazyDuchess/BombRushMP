c95c56e96e77dba06c71b2c24f32ff02491dffb0
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-07 18:58:26 -0300

    Add candyman board by hellfiend

--- BombRushMP.Plugin/MPUnlockManager.cs
+++ BombRushMP.Plugin/MPUnlockManager.cs
@@ -31,6 +31,7 @@ namespace BombRushMP.Plugin
             RegisterUnlock(new MPSkateboardSkin("Aqua", "hello i'm lazy duchess", Animator.StringToHash("aquaskateboard"), true, GetTextureFromAssets("AquaSkateboardTex")));
             RegisterUnlock(new MPSkateboardSkin("Rave", "yeaaa this one don't need no trucks", Animator.StringToHash("raveskateboard"), true, GetMaterialFromAssets("RaveboardMat"), GetMeshFromAssets("RaveboardMesh")));
             RegisterUnlock(new MPSkateboardSkin("Futurism", "how the fuck do you pop the tail", Animator.StringToHash("fismboard"), true, GetMaterialFromAssets("fismboardMat"), GetMeshFromAssets("fismboardMesh")));
+            RegisterUnlock(new MPSkateboardSkin("Candyman", "i love candy... and men!", Animator.StringToHash("candymanboard"), true, GetTextureFromAssets("CandymanBoard")));
         }
 
         private Material GetMaterialFromAssets(string name)
