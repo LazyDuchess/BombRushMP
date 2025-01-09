c7fcb597fc4bd9b15f8b07353b9bc06de9c39671
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-09 03:44:37 -0300

    Create ServerAuthResponse.cs

---
+++ BombRushMP.Common/Packets/ServerAuthResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombRushMP.Common.Packets
+{
+    public class ServerAuthResponse : Packet
+    {
+        public override Packets PacketId => Packets.ServerAuthResponse;
+        public UserKinds UserKind = UserKinds.Player;
+
+        public override void Read(BinaryReader reader)
+        {
+            UserKind = (UserKinds)reader.ReadInt32();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            writer.Write((int)UserKind);
+        }
+    }
+}
