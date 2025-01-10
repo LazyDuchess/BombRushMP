ade7e70e607d501472b221d38dca16d8781d32b2
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-09 22:18:14 -0300

    badge adjustments, don't let players w special skins use toilet

---
+++ BombRushMP.Plugin/Patches/PublicToiletPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Reptile;
+
+namespace BombRushMP.Plugin.Patches
+{
+    [HarmonyPatch(typeof(PublicToilet))]
+    internal static class PublicToiletPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(PublicToilet.DoSequence))]
+        private static bool DoSequence_Prefix()
+        {
+            var playerComp = PlayerComponent.GetLocal();
+            if (playerComp.SpecialSkin != Common.SpecialSkins.None)
+                return false;
+            return true;
+        }
+    }
+}
