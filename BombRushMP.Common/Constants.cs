19a99d4fb1d6bb735f68164dcc9362f3ca50169d
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-11 21:26:24 -0300

    increase protocolver

--- BombRushMP.Common/Constants.cs
+++ BombRushMP.Common/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const uint ProtocolVersion = 11;
+        public const uint ProtocolVersion = 12;
         public const float DefaultNetworkingTickRate = 0.03125f;
         public const float ScoreBattleDuration = 180f;
         public const int MaxPayloadSize = 10000;
