97b085ead8552f6a22a46841da695b15d34ecd5f
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-12 01:53:48 -0300

    implement compression and bitfield

--- BombRushMP.Common/Bitfield.cs
+++ BombRushMP.Common/Bitfield.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,55 @@ namespace BombRushMP.Common
             set => SetValue(key, value);
         }
         private bool[] _values = [];
-        public Bitfield()
-        {
 
+        public static Bitfield ReadShort(BinaryReader reader)
+        {
+            var field = new Bitfield();
+            var val = reader.ReadInt16();
+            var bitcount = sizeof(short) * 8;
+            field._values = new bool[bitcount];
+            for (var i = 0; i < bitcount; i++)
+            {
+                var mask = (short)(1 << i);
+                field._values[i] = (val & mask) != 0;
+            }
+            return field;
+        }
+
+        public static Bitfield ReadInteger(BinaryReader reader)
+        {
+            var field = new Bitfield();
+            var val = reader.ReadInt32();
+            var bitcount = sizeof(int) * 8;
+            field._values = new bool[bitcount];
+            for(var i = 0; i < bitcount; i++)
+            {
+                var mask = (1 << i);
+                field._values[i] = (val & mask) != 0;
+            }
+            return field;
+        }
+
+        public void WriteShort(BinaryWriter writer)
+        {
+            short shortVal = 0;
+            for (var i = 0; i < _values.Length; i++)
+            {
+                if (_values[i])
+                    shortVal |= (short)(1 << i);
+            }
+            writer.Write(shortVal);
+        }
+
+        public void WriteInteger(BinaryWriter writer)
+        {
+            var intVal = 0;
+            for(var i=0;i<_values.Length;i++)
+            {
+                if (_values[i])
+                    intVal |= (1 << i);
+            }
+            writer.Write(intVal);
         }
 
         public bool GetValue(Enum index)
---
+++ BombRushMP.Common/Compression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombRushMP.Common
+{
+    public static class Compression
+    {
+        public static sbyte CompressNormal(float normalValue)
+        {
+            return (sbyte)(normalValue * sbyte.MaxValue);
+        }
+
+        public static float DecompressNormal(sbyte compressedValue)
+        {
+            return (float)compressedValue / sbyte.MaxValue;
+        }
+    }
+}
