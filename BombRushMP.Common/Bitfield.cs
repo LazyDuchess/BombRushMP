575d69cef07348b131276799c5ce9db838e921c6
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-12 01:44:38 -0300

    Create Bitfield.cs

---
+++ BombRushMP.Common/Bitfield.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombRushMP.Common
+{
+    public class Bitfield
+    {
+        public bool this[Enum key]
+        {
+            get => GetValue(key);
+            set => SetValue(key, value);
+        }
+        private bool[] _values = [];
+        public Bitfield()
+        {
+
+        }
+
+        public bool GetValue(Enum index)
+        {
+            return _values[Convert.ToInt32(index)];
+        }
+
+        public void SetValue(Enum index, bool value)
+        {
+            _values[Convert.ToInt32(index)] = value;
+        }
+    }
+}
