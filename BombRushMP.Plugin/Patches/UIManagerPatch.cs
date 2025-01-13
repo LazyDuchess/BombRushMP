a3c663bd90a5afd82ec53af596fb0eb421b38b03
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-13 10:37:13 -0300

    don't allow pausing while in a gamemode

---
+++ BombRushMP.Plugin/Patches/UIManagerPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reptile;
+using HarmonyLib;
+
+namespace BombRushMP.Plugin.Patches
+{
+    [HarmonyPatch(typeof(UIManager))]
+    internal static class UIManagerPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(UIManager.ShowPauseMenu))]
+        private static bool ShowPauseMenu_Prefix()
+        {
+            var currentLobby = ClientController.Instance.ClientLobbyManager.CurrentLobby;
+            if (currentLobby != null && currentLobby.InGame)
+                return false;
+            return true;
+        }
+    }
+}
