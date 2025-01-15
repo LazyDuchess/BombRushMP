a0b98c8c25ef90b0473d113a33653d206b5706d2
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-15 11:47:52 -0300

    don't remove taxi sign in lobbies

---
+++ BombRushMP.Plugin/Patches/NPCPatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Reptile;
+namespace BombRushMP.Plugin.Patches
+{
+    [HarmonyPatch(typeof(NPC))]
+    internal static class NPCPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(NPC.SetAvailable))]
+        private static bool SetAvailable_Prefix(bool set, NPC __instance)
+        {
+            var taxi = __instance.GetComponent<Taxi>();
+            if (taxi == null) return true;
+            var currentEncounter = WorldHandler.instance.currentEncounter;
+            if (!set && currentEncounter != null && currentEncounter is ProxyEncounter && __instance.available)
+            {
+                __instance.available = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
