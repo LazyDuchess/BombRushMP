33b01a4c56f697580950598f82c3569a1c7cf90d
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-16 15:20:53 -0300

    Add teams

---
+++ BombRushMP.Plugin/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BombRushMP.Plugin
+{
+    public class Team
+    {
+        public string Name;
+        public Color Color;
+
+        public Team(string name, Color color)
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+}
---
+++ BombRushMP.Plugin/TeamManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BombRushMP.Plugin
+{
+    public static class TeamManager
+    {
+        public static Team[] Teams = [
+            new Team("Red Team", Color.red),
+            new Team("Blue Team", Color.blue),
+            new Team("Yellow Team", Color.yellow),
+            new Team("Green Team", Color.green)
+        ];
+    }
+}
