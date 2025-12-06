7e38414c45dfe780f95e7f9ecf486c9b6ada5fb5
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2025-12-05 21:28:58 -0300

    turn around while idle and on foot to aim

--- BombRushMP.Plugin/PropHuntPlayer.cs
+++ BombRushMP.Plugin/PropHuntPlayer.cs
@@ -42,13 +42,19 @@ namespace BombRushMP.Plugin
             playerForwardFlat.y = 0f;
             playerForwardFlat = playerForwardFlat.normalized;
 
-            if (Vector3.Dot(camForwardFlat, playerForwardFlat) < 0) return true;
+            if (Vector3.Dot(camForwardFlat, playerForwardFlat) < -0.1) return true;
 
             return false;
         }
 
+        private bool CanTurnToAim()
+        {
+            return (!_player.usingEquippedMovestyle && _player.ability == null && _player.GetTotalSpeed() <= 0.5f);
+        }
+
         private void LateUpdate()
         {
+            var canTurnToAIm = CanTurnToAim();
             _aiming = false;
             if (_player.sprayButtonHeld)
             {
@@ -60,7 +66,7 @@ namespace BombRushMP.Plugin
                 _cameraAimAmount = Mathf.Lerp(_cameraAimAmount, 0f, _cameraAimSpeed * Time.deltaTime);
             }
 
-            if (_player.sprayButtonHeld && _player.ability == null && !IsAimingBackwards())
+            if ((_player.sprayButtonHeld && _player.ability == null && !IsAimingBackwards()) || canTurnToAIm)
             {
                 _spineAimAmount = Mathf.Lerp(_spineAimAmount, 1f, _spineAimSpeed * Time.deltaTime);
                 _player.anim.Play(_player.canSprayHash, 1, 0.5f);
@@ -70,21 +76,30 @@ namespace BombRushMP.Plugin
                 _spineAimAmount = Mathf.Lerp(_spineAimAmount, 0f, _spineAimSpeed * Time.deltaTime);
             }
 
+            if (_aiming && IsAimingBackwards() && canTurnToAIm)
+            {
+                var camForwardFlat = _cam.transform.forward;
+                camForwardFlat.y = 0f;
+                camForwardFlat = camForwardFlat.normalized;
+                var targetRot = Quaternion.LookRotation(camForwardFlat, Vector2.up);
+                _player.SetRotation(targetRot);
+            }
+
             _player.spraycanLayerWeight = _spineAimAmount;
             _player.anim.SetLayerWeight(1, _spineAimAmount);
             _player.anim.SetLayerWeight(2, _spineAimAmount);
 
-            var camPos = _cam.transform.position;
-            camPos += Vector3.up * 0.2f;
-            camPos += _cam.transform.right * 0.2f;
-            camPos += _cam.transform.forward * 2f;
+            var camPos = _player.transform.position;
+            camPos += Vector3.up * 1.5f;
+            camPos += _cam.transform.right * 0.5f;
+            camPos -= _cam.transform.forward * 1f;
 
             var spine = _player.characterVisual.head.parent.parent;
             var localDir = spine.InverseTransformDirection(_cam.transform.forward);
             var delta = Quaternion.FromToRotation(Vector3.forward, localDir);
             var finalRot = spine.localRotation * delta;
 
-            spine.localRotation = Quaternion.Slerp(spine.localRotation, finalRot, _spineAimAmount);
+            spine.localRotation = Quaternion.Slerp(spine.localRotation, finalRot, _spineAimAmount * 0.7f);
             _cam.transform.position = Vector3.Lerp(_cam.transform.position, camPos, _cameraAimAmount);
         }
     }
