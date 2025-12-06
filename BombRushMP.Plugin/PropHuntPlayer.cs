7a2819a8f3bfc2646b5dcf0e0d06a888eeb004f8
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2025-12-05 22:36:52 -0300

    make turn smoother

--- BombRushMP.Plugin/PropHuntPlayer.cs
+++ BombRushMP.Plugin/PropHuntPlayer.cs
@@ -21,6 +21,12 @@ namespace BombRushMP.Plugin
 
         private bool _aiming = false;
 
+        private float _turnSpeed = 1000f;
+        private Vector3 _targetFacing = Vector3.zero;
+
+        private float _backwardsThreshold = -0.1f;
+        private float _turnThreshold = 0.2f;
+
         private void Awake()
         {
             _player = GetComponent<Player>();
@@ -32,41 +38,58 @@ namespace BombRushMP.Plugin
             return WorldHandler.instance.GetCurrentPlayer().GetComponent<PropHuntPlayer>();
         }
 
-        private bool IsAimingBackwards()
+        private bool IsAimingBackwards(Vector3 forward, float thresh)
         {
             var camForwardFlat = _cam.transform.forward;
             camForwardFlat.y = 0f;
             camForwardFlat = camForwardFlat.normalized;
 
-            var playerForwardFlat = _player.transform.forward;
+            var playerForwardFlat = forward;
             playerForwardFlat.y = 0f;
             playerForwardFlat = playerForwardFlat.normalized;
 
-            if (Vector3.Dot(camForwardFlat, playerForwardFlat) < -0.1) return true;
+            if (Vector3.Dot(camForwardFlat, playerForwardFlat) < thresh) return true;
 
             return false;
         }
 
+        private bool PressingMovementKeys()
+        {
+            return _player.moveInput.magnitude > 0f;
+        }
+
         private bool CanTurnToAim()
         {
-            return (!_player.usingEquippedMovestyle && _player.ability == null && _player.GetTotalSpeed() <= 0.5f);
+            return (!_player.usingEquippedMovestyle && _player.ability == null && _player.GetTotalSpeed() <= 0.5f && !PressingMovementKeys());
+        }
+
+        private void FixedUpdate()
+        {
+            if (_aiming && CanTurnToAim())
+            {
+                var flatCurrentForward = _player.transform.forward;
+                flatCurrentForward.y = 0f;
+                flatCurrentForward = flatCurrentForward.normalized;
+
+                var yawDelta = Vector3.SignedAngle(flatCurrentForward, _targetFacing, Vector3.up);
+                _player.SetRotation(_player.transform.rotation * Quaternion.Euler(0f, Mathf.Clamp(yawDelta, -_turnSpeed * Time.deltaTime, _turnSpeed * Time.deltaTime), 0f));
+            }
         }
 
         private void LateUpdate()
         {
             var canTurnToAIm = CanTurnToAim();
-            _aiming = false;
-            if (_player.sprayButtonHeld)
+            _aiming = _player.sprayButtonHeld;
+            if (_aiming)
             {
                 _cameraAimAmount = Mathf.Lerp(_cameraAimAmount, 1f, _cameraAimSpeed * Time.deltaTime);
-                _aiming = true;
             }
             else
             {
                 _cameraAimAmount = Mathf.Lerp(_cameraAimAmount, 0f, _cameraAimSpeed * Time.deltaTime);
             }
 
-            if ((_player.sprayButtonHeld && _player.ability == null && !IsAimingBackwards()) || canTurnToAIm)
+            if (_aiming && _player.ability == null && (!IsAimingBackwards(_player.transform.forward, _backwardsThreshold) || canTurnToAIm))
             {
                 _spineAimAmount = Mathf.Lerp(_spineAimAmount, 1f, _spineAimSpeed * Time.deltaTime);
                 _player.anim.Play(_player.canSprayHash, 1, 0.5f);
@@ -76,13 +99,12 @@ namespace BombRushMP.Plugin
                 _spineAimAmount = Mathf.Lerp(_spineAimAmount, 0f, _spineAimSpeed * Time.deltaTime);
             }
 
-            if (_aiming && IsAimingBackwards() && canTurnToAIm)
+            if (_aiming && IsAimingBackwards(_targetFacing, _turnThreshold) && canTurnToAIm)
             {
                 var camForwardFlat = _cam.transform.forward;
                 camForwardFlat.y = 0f;
                 camForwardFlat = camForwardFlat.normalized;
-                var targetRot = Quaternion.LookRotation(camForwardFlat, Vector2.up);
-                _player.SetRotation(targetRot);
+                _targetFacing = camForwardFlat;
             }
 
             _player.spraycanLayerWeight = _spineAimAmount;
@@ -101,6 +123,15 @@ namespace BombRushMP.Plugin
 
             spine.localRotation = Quaternion.Slerp(spine.localRotation, finalRot, _spineAimAmount * 0.7f);
             _cam.transform.position = Vector3.Lerp(_cam.transform.position, camPos, _cameraAimAmount);
+
+            
+
+            if (!_aiming || !canTurnToAIm || PressingMovementKeys())
+            {
+                _targetFacing = _player.transform.forward;
+                _targetFacing.y = 0f;
+                _targetFacing = _targetFacing.normalized;
+            }
         }
     }
 }
