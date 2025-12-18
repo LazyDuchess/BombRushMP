f2a2932e621346d5b3e7cec0354279ce96472f43
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2025-12-18 00:13:41 -0300

    improve build system and stuff

--- BombRushMP.Editor/Assets/Scripts/Editor/BundleBuilder.cs
+++ BombRushMP.Editor/Assets/Scripts/Editor/BundleBuilder.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Linq;
 
 public class BundleBuilder : MonoBehaviour
 {
-    [MenuItem("BombRushMP/Build Asset Bundles")]
-    private static void BuildAssetBundle()
+    [MenuItem("BombRushMP/Build Asset Bundle (Release)")]
+    private static void BuildAssetBundleRelease()
     {
-        Directory.CreateDirectory("Build");
-        BuildPipeline.BuildAssetBundles("Build", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        BuildAssets(true);
+    }
+
+    [MenuItem("BombRushMP/Build Asset Bundle (Debug)")]
+    private static void BuildAssetBundleDebug()
+    {
+        BuildAssets(false);
+    }
+
+    private const string BundleName = "assets";
+    private const string OutputPath = "Build";
+
+    private static void BuildAssets(bool compressed)
+    {
+        Directory.CreateDirectory(OutputPath);
+        var assets = AssetDatabase.GetAssetPathsFromAssetBundle(BundleName);
+        var deps = AssetDatabase.GetDependencies(assets, true);
+        deps = deps.Where(dep => AssetDatabase.GetImplicitAssetBundleName(dep) == string.Empty && !dep.EndsWith(".cs")).ToArray();
+        var finalAssets = assets.Concat(deps).Distinct().ToArray();
+        var build = new AssetBundleBuild
+        {
+            assetBundleName = BundleName,
+            assetNames = finalAssets
+        };
+        BuildPipeline.BuildAssetBundles(OutputPath, new[] { build }, compressed ? BuildAssetBundleOptions.None : BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
     }
 }
---
+++ BombRushMP.Mono/Runtime/ToggleInteractable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if PLUGIN
+using CommonAPI;
+using Reptile;
+#endif
+using UnityEngine;
+
+namespace BombRushMP.Mono
+{
+#if PLUGIN
+    public class ToggleInteractable : CustomInteractable
+#else
+    public class ToggleInteractable : MonoBehaviour
+#endif
+    {
+        public AudioSource ToggleOnSFX = null;
+        public AudioSource ToggleOffSFX = null;
+        public GameObject ObjectToToggleOn = null;
+        public GameObject ObjectToToggleOff = null;
+        public bool StartsOn = false;
+        private bool _isOn = false;
+
+#if PLUGIN
+        private void Awake()
+        {
+            PlacePlayerAtSnapPosition = false;
+            _isOn = StartsOn;
+            UpdateState();
+        }
+
+        public override void Interact(Player player)
+        {
+            _isOn = !_isOn;
+            if (_isOn)
+            {
+                if (ToggleOnSFX != null)
+                    ToggleOnSFX.Play();
+            }
+            else
+            {
+                if (ToggleOffSFX != null)
+                    ToggleOffSFX.Play();
+            }
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            if (_isOn)
+            {
+                ObjectToToggleOn.gameObject.SetActive(true);
+                ObjectToToggleOff.gameObject.SetActive(false);
+            }
+            else
+            {
+                ObjectToToggleOn.gameObject.SetActive(false);
+                ObjectToToggleOff.gameObject.SetActive(true);
+            }
+        }
+#endif
+    }
+}
