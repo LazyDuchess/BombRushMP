aed9e35e86d989bb580442899be5da1e73d7cb6b
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2025-12-21 13:29:56 -0300

    add error check

---
+++ BombRushMP.Mono/Runtime/NPCVisibilityController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+#if PLUGIN
+using Reptile;
+#endif
+
+namespace BombRushMP.Mono.Runtime
+{
+    public class NPCVisibilityController : MonoBehaviour
+    {
+        public GameObject NPC;
+        public int AssociatedCharacterIndex = -1;
+
+#if PLUGIN
+        private Characters AssociatedCharacter => (Characters)AssociatedCharacterIndex;
+        private void Update()
+        {
+            var shouldBeVisible = true;
+            var player = WorldHandler.instance.GetCurrentPlayer();
+            if (player != null && player.character == AssociatedCharacter)
+            {
+                shouldBeVisible = false;
+            }
+            else if (WorldHandler.instance.currentEncounter != null)
+            {
+                shouldBeVisible = false;
+            }
+
+            if (NPC.activeSelf != shouldBeVisible)
+            {
+                NPC.SetActive(shouldBeVisible);
+            }
+        }
+#endif
+    }
+}
