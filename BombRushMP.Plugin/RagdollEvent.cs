a65871cd2dda0555240d58f77a2be8c595fc2e07
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2026-07-09 09:14:07 -0300

    create ragdoll packet classes

---
+++ BombRushMP.Plugin/RagdollEvent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombRushMP.Plugin
+{
+    public class RagdollEvent
+    {
+        public enum Events
+        {
+            Start,
+            Stop
+        }
+        public Events Event = Events.Start;
+        public RagdollState State = null;
+
+        public RagdollEvent()
+        {
+
+        }
+
+        public RagdollEvent(Events ev){
+            Event = ev;
+        }
+
+        public RagdollEvent(Events ev, RagdollState state)
+        {
+            Event = ev;
+            State = state;
+        }
+
+        public void Read(BinaryReader reader)
+        {
+            var version = reader.ReadByte();
+            Event = (Events)reader.ReadInt32();
+            var hasState = reader.ReadBoolean();
+            if (hasState)
+            {
+                State = new RagdollState();
+                State.Read(reader);
+            }
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write((byte)0);
+            writer.Write((int)Event);
+            writer.Write(State != null);
+            if (State != null)
+            {
+                State.Write(writer);
+            }
+        }
+    }
+}
---
+++ BombRushMP.Plugin/RagdollState.cs
@@ -0,0 +1,47 @@
+﻿using BombRushMP.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombRushMP.Plugin
+{
+    public class RagdollState
+    {
+        public List<Quaternion> LimbRotations = new();
+
+        public RagdollState()
+        {
+
+        }
+
+        public RagdollState(List<Quaternion> limbRotations)
+        {
+            LimbRotations = limbRotations;
+        }
+
+        public void Read(BinaryReader reader)
+        {
+            LimbRotations.Clear();
+            var version = reader.ReadByte();
+            var limbAmount = (int)reader.ReadByte();
+            for(var i = 0; i < limbAmount; i++)
+            {
+                LimbRotations.Add(Compression.ReadCompressedQuaternion(reader));
+            }
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write((byte)0);
+            writer.Write((byte)LimbRotations.Count);
+            foreach(var rot in LimbRotations)
+            {
+                Compression.WriteCompressedQuaternion(rot, writer);
+            }
+        }
+    }
+}
