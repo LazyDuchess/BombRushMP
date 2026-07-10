6b317de80e74abf2377514dbf29da0884ade2464
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2026-07-10 13:37:30 -0300

    Fix effect related memory leak

---
+++ BombRushMP.Plugin/Patches/OneOffVisualEffectPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Reptile;
+using HarmonyLib;
+
+namespace BombRushMP.Plugin.Patches
+{
+    [HarmonyPatch(typeof(OneOffVisualEffect))]
+    internal static class OneOffVisualEffectPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(OneOffVisualEffect.Awake))]
+        private static void Awake_Postfix(OneOffVisualEffect __instance)
+        {
+            if (__instance.anim != null)
+            {
+                __instance.anim.cullingType = AnimationCullingType.AlwaysAnimate;
+            }
+        }
+    }
+}
