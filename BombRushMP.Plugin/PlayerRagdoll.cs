ce364b372667263f0173f442e81c6d230056ba3a
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2026-07-12 15:29:00 -0300

    look for ragdoll bones using parents instead of hardcoded names.

--- BombRushMP.Plugin/PlayerRagdoll.cs
+++ BombRushMP.Plugin/PlayerRagdoll.cs
@@ -363,64 +363,94 @@ namespace BombRushMP.Plugin
             Visual = player.Player.characterVisual;
             Limbs.Clear();
 
-            var root = Visual.root;
-            if (root == null) return;
+            try
+            {
+                var root = Visual.root;
+                if (root == null) return;
 
-            var s1 = root.transform.FindRecursive("s1");
-            if (s1 == null) return;
-            var s2 = root.transform.FindRecursive("s2");
-            if (s2 == null) return;
+                var leg2l = root.transform.FindRecursive("leg2l");
+                if (leg2l == null) return;
 
-            var head = root.transform.FindRecursive("head");
-            if (head == null) return;
+                var leg1l = leg2l.parent;
+                if (leg1l == null) return;
 
-            var leg1l = root.transform.FindRecursive("leg1l");
-            if (leg1l == null) return;
-            var leg2l = root.transform.FindRecursive("leg2l");
-            if (leg2l == null) return;
+                var pelvis = leg1l.parent;
+                if (pelvis == null) return;
 
-            var leg1r = root.transform.FindRecursive("leg1r");
-            if (leg1r == null) return;
-            var leg2r = root.transform.FindRecursive("leg2r");
-            if (leg2r == null) return;
+                var head = root.transform.FindRecursive("head");
+                if (head == null) return;
 
-            var arm1l = root.transform.FindRecursive("arm1l");
-            if (arm1l == null) return;
-            var arm2l = root.transform.FindRecursive("arm2l");
-            if (arm2l == null) return;
+                var curp = head;
+                var nextp = head.parent;
 
-            var arm1r = root.transform.FindRecursive("arm1r");
-            if (arm1r == null) return;
-            var arm2r = root.transform.FindRecursive("arm2r");
-            if (arm2r == null) return;
+                while (nextp != null && nextp != pelvis)
+                {
+                    curp = nextp;
+                    nextp = curp.parent;
+                }
 
-            Valid = true;
+                if (nextp == null) return;
 
-            var rootLimb = new Limb(root, Limb.LimbTypes.Root, this);
+                var s1 = curp;
 
-            var s1Limb = new Limb(s1, Limb.LimbTypes.Spine, this, rootLimb);
-            var s2Limb = new Limb(s2, Limb.LimbTypes.Spine, this, s1Limb);
+                if (s1 == null) return;
+                var neck = head.parent;
+                if (neck == null) return;
+                var s2 = neck.parent;
+                if (s2 == null) return;
 
-            var headLimb = new Limb(head, Limb.LimbTypes.Head, this, s2Limb);
+                var leg2r = root.transform.FindRecursive("leg2r");
+                if (leg2r == null) return;
 
-            var leg1lLimb = new Limb(leg1l, Limb.LimbTypes.Leg, this, rootLimb);
-            var leg2lLimb = new Limb(leg2l, Limb.LimbTypes.Leg, this, leg1lLimb);
+                var leg1r = leg2r.parent;
+                if (leg1r == null) return;
 
-            var leg1rLimb = new Limb(leg1r, Limb.LimbTypes.Leg, this, rootLimb);
-            var leg2rLimb = new Limb(leg2r, Limb.LimbTypes.Leg, this, leg1rLimb);
+                var handr = root.transform.FindRecursive("handr");
+                var handl = root.transform.FindRecursive("handl");
 
-            var arm1lLimb = new Limb(arm1l, Limb.LimbTypes.Arm, this, s2Limb);
-            var arm2lLimb = new Limb(arm2l, Limb.LimbTypes.Arm, this, arm1lLimb);
+                if (handr == null) return;
+                if (handl == null) return;
 
-            var arm1rLimb = new Limb(arm1r, Limb.LimbTypes.Arm, this, s2Limb);
-            var arm2rLimb = new Limb(arm2r, Limb.LimbTypes.Arm, this, arm1rLimb);
+                var arm2l = handl.parent;
+                if (arm2l == null) return;
 
-            Limbs = [
-                rootLimb, 
-                s1Limb, 
-                s2Limb, 
-                headLimb, 
-                leg1lLimb, 
+                var arm1l = arm2l.parent;
+                if (arm1l == null) return;
+
+                var arm2r = handr.parent;
+                if (arm2r == null) return;
+
+                var arm1r = arm2r.parent;
+                if (arm1r == null) return;
+
+
+                Valid = true;
+
+                var rootLimb = new Limb(pelvis, Limb.LimbTypes.Root, this);
+
+                var s1Limb = new Limb(s1, Limb.LimbTypes.Spine, this, rootLimb);
+                var s2Limb = new Limb(s2, Limb.LimbTypes.Spine, this, s1Limb);
+
+                var headLimb = new Limb(head, Limb.LimbTypes.Head, this, s2Limb);
+
+                var leg1lLimb = new Limb(leg1l, Limb.LimbTypes.Leg, this, rootLimb);
+                var leg2lLimb = new Limb(leg2l, Limb.LimbTypes.Leg, this, leg1lLimb);
+
+                var leg1rLimb = new Limb(leg1r, Limb.LimbTypes.Leg, this, rootLimb);
+                var leg2rLimb = new Limb(leg2r, Limb.LimbTypes.Leg, this, leg1rLimb);
+
+                var arm1lLimb = new Limb(arm1l, Limb.LimbTypes.Arm, this, s2Limb);
+                var arm2lLimb = new Limb(arm2l, Limb.LimbTypes.Arm, this, arm1lLimb);
+
+                var arm1rLimb = new Limb(arm1r, Limb.LimbTypes.Arm, this, s2Limb);
+                var arm2rLimb = new Limb(arm2r, Limb.LimbTypes.Arm, this, arm1rLimb);
+
+                Limbs = [
+                    rootLimb,
+                s1Limb,
+                s2Limb,
+                headLimb,
+                leg1lLimb,
                 leg2lLimb,
                 leg1rLimb,
                 leg2rLimb,
@@ -428,7 +458,14 @@ namespace BombRushMP.Plugin
                 arm2lLimb,
                 arm1rLimb,
                 arm2rLimb
-            ];
+                ];
+            }
+            catch(Exception e)
+            {
+                Debug.LogError($"FAILED TO CREATE RAGDOLL FOR {Visual.name}!!");
+                Debug.LogException(e);
+                Valid = false;
+            }
         }
     }
 }
